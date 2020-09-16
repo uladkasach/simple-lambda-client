54397698166dc64dd381682eefae4675f06c6f37
Ulad Kasach <uladkasach@gmail.com>
2020-09-16 05:19:01 -0500

    fix(errorhandling): add more details and typing around LambdaInvocationError

--- src/errors.ts
+++ src/errors.ts
@@ -9,9 +9,27 @@ export class UnsuccessfulStatusCodeError extends Error {
 }
 
 export class LambdaInvocationError extends Error {
+  /**
+   * the name of the lambda that returned the error
+   */
   public lambda: string;
-  public response: any;
+
+  /**
+   * invocation errors that we detect contain one of the following three keys
+   *
+   * they are all optional, because some error responses contain one but not all
+   */
+  public response: {
+    errorMessage?: string;
+    errorType?: string;
+    stackTrace?: string;
+  };
+
+  /**
+   * the event that the lambda was invoked with
+   */
   public event: any;
+
   constructor({ lambda, response, event }: { lambda: string; response: any; event: any }) {
     const message = `An error was returned as the lambda invocation response for the lambda '${lambda}': "${response.errorMessage}". See error properties for more details.`;
     super(message);
